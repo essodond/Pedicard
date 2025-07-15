985c45758f9d4cf9a7bd4720e99af541300e48fc
elisee <makpassimelisee@gmail.com>
2025-07-15 10:44:16 +0000

    creation des services

--- pedicard/settings.py
+++ pedicard/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ppe',
         'USER': 'postgres',
-        'PASSWORD': 'wil',
+        'PASSWORD': 'Helios1290',
         'HOST': 'localhost',
         'PORT': '5432',
     }
